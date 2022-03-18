9a02446360c052e99c849fe06a6f8ee53905fd3e
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-18 15:24:49 -0700

    update engineer controller, add create/index view

---
+++ Factory/Controllers/EngineersController.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class EngineersController : Controller
+  {
+    private readonly FactoryContext _db;
+    public EngineersController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Engineers.ToList());
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.MachineId = new SelectList(_db.Machines, "MachineId", "Name");
+      return View();
+    } 
+    [HttpPost]
+    public ActionResult Create(Engineer engineer)
+    {
+      _db.Engineers.Add(engineer);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+
+  
+}
