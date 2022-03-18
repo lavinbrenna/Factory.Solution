a8e9bbb4e87d44329fbf1574b3658ebb8c1261eb
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-18 13:25:31 -0700

    add machine details route

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -28,7 +28,6 @@ namespace Factory.Controllers
     {
       _db.Machines.Add(machine);
       _db.SaveChanges();
-      machine.InspectionDate = machine.InspectionDate.Date;
       return RedirectToAction("Index");
     }
 
