4e64d9d4751831c2673e67db3ebd73d724d911f6
Brenna Lavin <lavinbrenna@gmail.com>
2022-03-18 10:48:04 -0700

    start machines controller

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,21 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Factory.Models;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Index()
+    {
+      return View(_db.Machines.ToList());
+    }
+  }
+}
